791f50395bd91e6df712b58ea0638a1146954b00
nelidita <nelidavalero06@gmail.com>
2021-08-26 00:45:33 -0500

    create react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Inicio from './components/incio'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Inicio></Inicio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/incio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Inicio() {
+
+
+
+  
+    return (
+      <div className="Inicio">
+        <h1>Califica tu comic favorito</h1>
+      </div>
+    );
+  }
+  
+  export default Inicio;
