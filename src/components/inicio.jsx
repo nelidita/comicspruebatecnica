88c3acfac1b6b34ad5086eb615a9cb43796e53aa
nelidita <nelidavalero06@gmail.com>
2021-08-27 15:42:13 -0500

    Mostrando titulo e imagen del comic y calificacion interactiva

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react'
+import "./inicio.css"
+import sad from '../img/sad.png'
+import meh from '../img/meh.png'
+import smile from '../img/smile.png'
+
 
 function Inicio() {
 
-fetch('http://localhost:5000/getData')
-.then(response => response.json())
-.then(data => console.log(data))
+  const [comic, setComic] = useState("")
+  const [calificacion, setCalificacion] = useState("Queremos saber tu calificación")
 
-  
-    return (
-      <div className="Inicio">
-        <h1>Califica tu comic favorito</h1>
-      </div>
-    );
+  useEffect(async () => {
+    const data = await fetch('http://localhost:5000/getData');
+    const comics = await data.json();
+    setComic(comics)
+  }, [])
+
+  const obtenerCalificacion = (event) => {
+    const tipoCalificacion = event.target.name
+    setCalificacion(tipoCalificacion)
   }
-  
-  export default Inicio;
+
+  return (
+    <Fragment>
+      <div className="Inicio">
+        <h1>Califica tu Comic</h1>
+        <h2>{comic.title}</h2>
+        <img src={comic.img} alt={comic.title} />
+        <div className="IconosCalificacion">
+          <img onClick={obtenerCalificacion} className="icon" src={sad} name="sad" />
+          <img onClick={obtenerCalificacion} className="icon" src={meh} name="meh" />
+          <img onClick={obtenerCalificacion} className="icon" src={smile} name="smile" />
+        </div>
+        <div>
+          {calificacion === "sad" ? <h3>No me gusta</h3>:""}
+          {calificacion === "meh" ? <h3>mmm Normal</h3>:""}
+          {calificacion === "smile" ? <h3>Me gusta mucho</h3>:""}
+        </div>
+
+      </div>
+    </Fragment>
+  );
+}
+
+export default Inicio;
